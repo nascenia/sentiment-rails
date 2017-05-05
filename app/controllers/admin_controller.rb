4bffe4d5f28257f13b0124473ecea0d2fad7f303
awlad <awlad@nascenia.com>
2017-05-05 17:21:08 +0600

    Fixed design and css issues

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,8 +5,8 @@ class AdminController < ApplicationController
   end
 
   def check_sentiments
-    start = Date.strptime(params[:start_date] || Date.today, '%m/%d/%Y')
-    end_date = Date.strptime(params[:end_date] || Date.today, '%m/%d/%Y')
+    start = DateTime.strptime(params[:start_date] , '%m/%d/%Y').beginning_of_day
+    end_date = DateTime.strptime(params[:end_date], '%m/%d/%Y').end_of_day
     user = params[:user]
     @tones = get_data(start, end_date, user)
   end
@@ -22,8 +22,8 @@ class AdminController < ApplicationController
 
   def get_data(start, end_date, user)
     user = User.find user
-    # text = user.messages.between(start, end_date).map{|m| m.body}.join(" ")
-    text = user.messages.map{|m| m.body}.join(" ")
+    text = user.messages.between(start, end_date).map{|m| m.body}.join(" ")
+    # text = user.messages.map{|m| m.body}.join(" ")
     return nil if text.empty?
     url = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19'
     auth = {:username => "abbb6f1f-b5f6-4f81-8c12-6bea6b7c6ffb", :password => "A7nIFyC3mcvy"}
