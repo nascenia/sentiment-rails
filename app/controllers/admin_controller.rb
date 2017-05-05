ed4315fece36b062d8683b07b585fc35775c66a0
awlad <awlad@nascenia.com>
2017-05-05 19:11:11 +0600

    Added bar chart

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,7 +8,19 @@ class AdminController < ApplicationController
     start = DateTime.strptime(params[:start_date] , '%m/%d/%Y').beginning_of_day
     end_date = DateTime.strptime(params[:end_date], '%m/%d/%Y').end_of_day
     user = params[:user]
-    @tones = get_data(start, end_date, user)
+    tones = get_data(start, end_date, user)
+    @chart_data = []
+    tones['document_tone']['tone_categories'].each do |data|
+      data['tones'].each do |tone_data|
+        if tone_data['score'] > 0.5
+          @chart_data << [tone_data['tone_name'], tone_data['score'].round(2)]
+        end
+      end
+    end
+    respond_to do |format|
+      format.js {render layout: false}
+    end
+
   end
 
   protected
@@ -22,8 +34,8 @@ class AdminController < ApplicationController
 
   def get_data(start, end_date, user)
     user = User.find user
+    @user = user
     text = user.messages.between(start, end_date).map{|m| m.body}.join(" ")
-    # text = user.messages.map{|m| m.body}.join(" ")
     return nil if text.empty?
     url = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19'
     auth = {:username => WATSON_USER, :password => WATSON_PASSWORD}
