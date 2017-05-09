f2f6764a9957ca935b7748b52476bb7a63033ad8
awlad <awlad@nascenia.com>
2017-05-09 18:36:18 +0600

    removed passenger in the gemfile for production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,6 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   Rails.application.config.action_cable.disable_request_forgery_protection = true
+  Rails.application.config.action_cable.allowed_request_origins = [".*"]
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   Rails.application.config.action_cable.disable_request_forgery_protection = true
+  Rails.application.config.action_cable.allowed_request_origins = [".*"]
 end
